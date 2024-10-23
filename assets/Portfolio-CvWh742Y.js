df979bc7bb4b1e04b3ba7715095b5ab2e17b7854
User Friendly <pivanov@user-friendly.dev>
2024-10-22 21:19:01 -0400

    release r77

---
+++ assets/Portfolio-CvWh742Y.js
@@ -0,0 +1,5 @@
+import{j as e}from"./index-DZxVDkqq.js";import"./services-Bu_7CigI.js";const o=()=>e.jsxs("div",{className:`
+		py-2 px-4
+		
+		rounded-xl bg-slate-300
+	`,children:[e.jsx("h2",{children:"Yar Pirate Ipsum"}),e.jsx("p",{children:"Grog tender snow wherry salmagundi swing the lead come about grog blossom ho bilge. Pirate black spot Sail ho Chain Shot rigging topsail Jolly Roger draught heave to marooned. Carouser fluke knave stern topsail driver Jack Ketch barkadeer hail-shot clap of thunder."}),e.jsx("p",{children:"Davy Jones' Locker warp bilged on her anchor brig Cat o'nine tails Spanish Main stern careen pillage booty. Piracy broadside plunder bring a spring upon her cable flogging crow's nest lee heave down scuttle six pounders. Keelhaul boatswain come about rope's end gaff quarterdeck grapple sutler gabion hardtack."}),e.jsx("p",{children:"Driver gangplank reef sails furl Admiral of the Black log fathom splice the main brace bilge water Sea Legs. Reef sails careen cable heave to execution dock aye brigantine quarterdeck spike handsomely. Topmast carouser scurvy stern fore fathom hearties hornswaggle Pirate Round belaying pin."})]});export{o as default};
