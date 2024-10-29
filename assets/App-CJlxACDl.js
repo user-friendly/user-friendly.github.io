ed9079252e98c4d228f62ff6b31942faf2ca8782
User Friendly <pivanov@user-friendly.dev>
2024-10-29 13:02:12 -0400

    release r93

---
+++ assets/App-CJlxACDl.js
@@ -0,0 +1,7 @@
+import{j as e,L as t}from"./index-v1JCV-fe.js";import"./services-C3bE939n.js";const n="text-green-600",s=`
+	p-2 material-symbols-outlined rounded-lg bg-green-100 select-none cursor-pointer
+	hover:bg-green-200 text-green-700
+`,o=()=>e.jsxs("div",{className:`
+		min-h-screen flex flex-col items-center
+		${n}
+	`,children:[e.jsx(t,{className:"text-sky-400 hover:underline",to:"/home",children:"Back To Main App"}),e.jsx("h1",{className:"my-4 text-5xl",children:"Capstone Project"}),e.jsx("div",{className:"my-4 text-xl font-playfair",children:"Font Playfair Display test. This here is some random text to test the given font."}),e.jsx("div",{className:"my-4 text-xl font-lato",children:"Font Lato test. This here is some random text to test the given font."}),e.jsxs("div",{children:[e.jsx("h2",{className:"my-4 text-4xl",children:"Icons"}),e.jsxs("div",{className:"flex items-center gap-4",children:[e.jsx("span",{className:`${s}`,children:"menu"}),e.jsx("span",{className:`${s} text-4xl`,children:"home"}),e.jsx("span",{className:`${s}`,children:"close"})]})]})]});export{o as default};
