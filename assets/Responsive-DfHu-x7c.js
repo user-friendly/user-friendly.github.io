037cb649dc799930326f5efbd38d3b00c130b02b
User Friendly <pivanov@user-friendly.dev>
2024-12-08 19:13:28 -0500

    release r175

--- assets/Responsive-DfHu-x7c.js
+++ assets/Responsive-DfHu-x7c.js
@@ -1,4 +1,4 @@
-import{j as e,A as i}from"./index-DT6KvBEu.js";import{C as n}from"./Collapsable-DPRM-dLg.js";import"./services-CQQVyGs2.js";const t=()=>e.jsxs("div",{className:`
+import{j as e,A as i}from"./index-CJWw79sX.js";import{C as n}from"./Collapsable-B1bNj0Fc.js";import"./services-CQQVyGs2.js";const t=()=>e.jsxs("div",{className:`
 		p-6 flex-1 flex flex-col justify-center items-center
 		text-gray-700
 		
