a364ffbc5e00bc4894a41cef46b8f00ad243c29b
User Friendly <pivanov@user-friendly.dev>
2024-12-09 10:07:36 -0500

    release r177

--- assets/Responsive-BXFEAUQ7.js
+++ assets/Responsive-BXFEAUQ7.js
@@ -1,4 +1,4 @@
-import{j as e,A as i}from"./index-DgxpeE32.js";import{C as n}from"./Collapsable-BQCavtxn.js";import"./services-CQQVyGs2.js";const t=()=>e.jsxs("div",{className:`
+import{j as e,A as i}from"./index-DPmdORZ2.js";import{C as n}from"./Collapsable-DtTdWJgN.js";import"./services-CQQVyGs2.js";const t=()=>e.jsxs("div",{className:`
 		p-6 flex-1 flex flex-col justify-center items-center
 		text-gray-700
 		
