6adac184dcc5324da672b4fe6cb65455edfe943e
User Friendly <pivanov@user-friendly.dev>
2024-12-13 18:32:00 -0500

    release r186

--- assets/Responsive-B9tEnJG9.js
+++ assets/Responsive-B9tEnJG9.js
@@ -1,4 +1,4 @@
-import{j as e,A as i}from"./index-BhOYj6Jb.js";import{C as n}from"./Collapsable-BB-htfcC.js";import"./services-CQQVyGs2.js";const t=()=>e.jsxs("div",{className:`
+import{j as e,A as i}from"./index-BnCVIr25.js";import{C as n}from"./Collapsable-Car2W1Xg.js";import"./services-CQQVyGs2.js";const t=()=>e.jsxs("div",{className:`
 		p-6 flex-1 flex flex-col justify-center items-center
 		text-gray-700
 		
