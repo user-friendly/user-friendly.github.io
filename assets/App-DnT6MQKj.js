77da2f719f15d8f3ca8a46c417329fb2e7b60c92
User Friendly <pivanov@user-friendly.dev>
2025-03-31 13:35:12 -0500

    release r193

--- assets/App-DnT6MQKj.js
+++ assets/App-DnT6MQKj.js
@@ -1,4 +1,4 @@
-import{r as c,j as e,A as o,L as i,B as r}from"./index-CQDuy8Y7.js";import{f as n}from"./services-D8YECjA7.js";const a=`
+import{r as c,j as e,A as o,L as i,B as r}from"./index-CY68mkt4.js";import{f as n}from"./services-D8YECjA7.js";const a=`
 	text-xl font-medium hover:underline
 	py-6 pl-8 text-violet-300
 	md:mx-4 md:text-inherit md:p-0
