53ec968edec33c07acc839bd1be4eebc5e216f6b
User Friendly <pivanov@user-friendly.dev>
2025-03-31 11:38:09 -0500

    release r192

---
+++ assets/Responsive-uD7kkrNv.js
@@ -0,0 +1,16 @@
+import{j as e,A as i}from"./index-CQDuy8Y7.js";import{C as n}from"./Collapsable-BfHGeu3E.js";import"./services-D8YECjA7.js";const t=()=>e.jsxs("div",{className:`
+		p-6 flex-1 flex flex-col justify-center items-center
+		text-gray-700
+		
+		min-w-80
+		
+		bg-red-400
+		sm:bg-amber-400
+		md:bg-green-400
+		lg:bg-teal-400
+		xl:bg-sky-400
+		2xl:bg-violet-400
+		
+		font-tiny5
+	`,children:[e.jsx("h2",{className:"text-4xl text-center",children:"Respoinsive Design Example"}),e.jsxs("div",{className:"my-4 text-xl flex flex-col",children:[e.jsx("strong",{children:"Screen size:"}),e.jsx("span",{className:"sm:hidden",children:"Default"}),e.jsx("span",{className:"hidden sm:max-md:inline",children:"Small 640px"}),e.jsx("span",{className:"hidden md:max-lg:inline",children:"Medium 768px"}),e.jsx("span",{className:"hidden lg:max-xl:inline",children:"Large 1024px"}),e.jsx("span",{className:"hidden xl:max-2xl:inline",children:"Extra Large 1280px"}),e.jsx("span",{className:"hidden 2xl:inline",children:"2 x Extra Large 1536px"})]})]}),a=`text-xl select-none cursor-pointer
+	px-2.5 py-0.5 rounded-xl bg-sky-400 transition-bg hover:bg-sky-500 hover:text-white`,m=({children:x})=>{let s=0;const l=[e.jsx(n,{collapse:!0,title:"Breakpoints",children:e.jsx(t,{})},s++),e.jsx(n,{collapse:!1,title:"Responsive",children:e.jsx("div",{className:"my-4 flex justify-center",children:e.jsx(i,{className:a,appid:"mobile",children:'To "Mobile Responsive Design" Site'})})},s++)];return e.jsxs("div",{className:"h-full flex flex-col justify-start items-strech",children:[e.jsx("h1",{className:"my-4 text-5xl self-center",children:"Responsive Design"}),l]})};export{m as default};
