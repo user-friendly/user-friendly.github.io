39d7caa768975c316f4f2d6b286f550940debbba
User Friendly <pivanov@user-friendly.dev>
2025-03-31 23:04:46 -0500

    release r194

--- assets/Responsive-Dln7bn88.js
+++ assets/Responsive-Dln7bn88.js
@@ -1,4 +1,4 @@
-import{j as e,A as i}from"./index-CY68mkt4.js";import{C as n}from"./Collapsable-Bitwb1yR.js";import"./services-D8YECjA7.js";const t=()=>e.jsxs("div",{className:`
+import{j as e,A as i}from"./index-yAZkXj3W.js";import{C as n}from"./Collapsable-zqJafXB0.js";import"./services-D8YECjA7.js";const t=()=>e.jsxs("div",{className:`
 		p-6 flex-1 flex flex-col justify-center items-center
 		text-gray-700
 		
