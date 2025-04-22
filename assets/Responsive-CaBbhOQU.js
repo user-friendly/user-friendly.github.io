d878ff8ebbcd2306dd1e2cee919e077e1a3606be
User Friendly <pivanov@user-friendly.dev>
2025-04-22 10:43:46 -0500

    release r205

--- assets/Responsive-CaBbhOQU.js
+++ assets/Responsive-CaBbhOQU.js
@@ -1,4 +1,4 @@
-import{j as e,A as i}from"./index-B6xhIFUX.js";import{C as n}from"./Collapsable-J4uzkVBO.js";import"./services-D8YECjA7.js";const t=()=>e.jsxs("div",{className:`
+import{j as e,A as i}from"./index-vxmDdVU5.js";import{C as n}from"./Collapsable-CH2AYBW6.js";import"./services-D8YECjA7.js";const t=()=>e.jsxs("div",{className:`
 		p-6 flex-1 flex flex-col justify-center items-center
 		text-gray-700
 		
